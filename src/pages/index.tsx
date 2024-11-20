43a18db37e045b6fb8f4be8952ac90812d3afb7a
arihant0924 <arihant.s@dataactions.ai>
2024-11-20 11:18:28 +0530

    minor changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 const AnalyticsPage = () => {
   return (
-    <div className="h-screen w-full flex justify-between items-center">
+    <div className="h-screen w-full flex justify-center items-center">
       <div className="p-16 rounded-md bg-white w-[50%] text-center">
         <p className="text-3xl font-medium">Get started...</p>
       </div>
