21a7f8a1a8f63020e45de83f8da1aac611b5ca21
thusharke25 <thushar.ke@dataactions.ai>
2024-11-27 16:33:34 +0530

    change config

--- src/pages/insights/organisers/players/[metricKey]/analysis/index.tsx
+++ src/pages/insights/organisers/players/[metricKey]/analysis/index.tsx
@@ -6,73 +6,68 @@ import {
 import { ValidSpanColumns } from "@/da-insight-kit/components/Insight";
 import { ChartTypes } from "@/da-insight-kit/constants/charts.contant";
 import { useRouter } from "next/router";
+import { FaChevronRight } from "react-icons/fa";
 
 const WORKSPACE_ID = "42eed85d-b1d7-4b8e-8621-1dfa79e72cf1";
+
+const config = {
+  active_users: [
+    {
+      id: 1,
+      metricKey: "revenue",
+      metricLabel: "Revenue",
+      title: "Revenue",
+      chartType: ChartTypes.SIMPLE_CHART,
+      metrics: [
+        {
+          metricKey: "revenue",
+          metricLabel: "Revenue",
+          chartType: ChartTypes.AREA,
+        },
+      ],
+      filters: {
+        revenue: {
+          compareWith: ["Min", "Max", "Average"],
+        },
+      },
+    },
+  ],
+};
 
 const AnalyticsPage = () => {
   const router = useRouter();
   return (
     <>
-      <DashboardLayout cols={ValidDashboardColumns.SIX} title="Reporting">
-        <Insight
-          workspaceId={WORKSPACE_ID}
-          title="Registrations"
-          type={ChartTypes.BIGNUMBERWITHTREND}
-          metrics={[
-            {
-              metricKey: "registrations",
-              metricLabel: "Registrations",
-            },
-          ]}
-          spanCols={ValidSpanColumns.TWO}
-          className="h-60"
-        />
-        <Insight
-          workspaceId={WORKSPACE_ID}
-          title="Active players"
-          type={ChartTypes.BIGNUMBERWITHTREND}
-          metrics={[
-            {
-              metricKey: "active_users",
-              metricLabel: "Active players",
-            },
-          ]}
-          spanCols={ValidSpanColumns.TWO}
-          onClick={() => {
-            router.push(`/insights/organisers/tournament/active_users/analysis`);
-          }}
-          className="h-60"
-        />
-        <Insight
-          workspaceId={WORKSPACE_ID}
-          title="Participation rate"
-          type={ChartTypes.BIGNUMBERWITHTREND}
-          metrics={[
-            {
-              metricKey: "participation_rate",
-              metricLabel: "Participation rate",
-            },
-          ]}
-          spanCols={ValidSpanColumns.TWO}
-          className="h-60"
-        />
-        <Insight
-          workspaceId={WORKSPACE_ID}
-          title="Revenue"
-          type={ChartTypes.BIGNUMBERWITHTREND}
-          metrics={[
-            {
-              metricKey: "revenue",
-              metricLabel: "Revenue",
-            },
-          ]}
-          onClick={() => {
-            router.push("/insights/organisers/tournament/revenue/analysis");
-          }}
-          spanCols={ValidSpanColumns.TWO}
-          className="h-60"
-        />
+      <DashboardLayout
+        cols={ValidDashboardColumns.SIX}
+        title={
+          <div className="pb-2">
+            <p>Analysis</p>
+          </div>
+        }
+      >
+        {config[router?.query?.metricKey]?.map((insight, index) => (
+          <Insight
+            workspaceId={WORKSPACE_ID}
+            title={insight.title}
+            key={index}
+            type={insight.chartType}
+            metrics={insight.metrics}
+            filters={insight.filters}
+            spanCols={ValidSpanColumns.THREE}
+            className="h-80"
+          />
+        ))}
       </DashboardLayout>
+      <p
+        className="p-5 underline cursor-pointer text-sm text-center text-gray-600 font-normal flex items-center gap-1 justify-center"
+        onClick={() => {
+          router.push("/insights/organisers/tournament/active_users/analysis/drilldown");
+        }}
+      >
+        Explore
+        <FaChevronRight size={12} />
+      </p>
     </>
   );
 };
