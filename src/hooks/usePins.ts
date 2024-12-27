734bef0b3145e380d8c5966e74dec35183c5b925
arihant0924 <arihant.s@dataactions.ai>
2024-12-27 16:19:51 +0530

    minor changes

--- src/hooks/usePins.ts
+++ src/hooks/usePins.ts
@@ -3,7 +3,7 @@ import { getPinsByWorkspaceIdAndUserId, IPin } from "../services/pins.svc";
 
 export function usePins(workspaceId: string) {
   const [loading, setLoading] = useState<boolean>(false);
-  const [pins, setPins] = useState<IPin[] | null>(null);
+  const [pins, setPins] = useState<IPin[]>([]);
 
   useEffect(() => {
     if (!workspaceId) return;
