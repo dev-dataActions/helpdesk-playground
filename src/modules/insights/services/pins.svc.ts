66448b61443507b782aeb2e2d4e42445b46c1505
arihant0924 <arihant.s@dataactions.ai>
2025-01-08 18:22:42 +0530

    added pin api

--- src/modules/insights/services/pins.svc.ts
+++ src/modules/insights/services/pins.svc.ts
@@ -1,31 +1,64 @@
 import { IInsight } from "da-insight-kit";
 
 export interface IPin {
-  id: string;
   data: IInsight;
   pin_id: string;
 }
 
 export const getPinsByUserId = async (userId: string): Promise<IPin[]> => {
   // Your SaaS app backend should have an API to fetch pins by workspaceId and userId
-  return [
-    {
-      id: "1",
-      data: {
-        chartType: "BIGNUMBER",
-        metrics: [
-          {
-            metricKey: "registrations",
-            metricLabel: "Registrations",
-          },
-        ],
-        title: "Registrations",
-      },
-      pin_id: "pin_27c18608-674f-4871-a7c6-6ef959995998",
-    },
-  ];
+  return fetch(
+    `https://backend.dataactions.ai/pin?workspace_id=arihant&user_id=${userId}`
+  )
+    .then((res) => res.json())
+    .then((data) => data)
+    .catch((err) => {
+      console.error(err);
+      return [];
+    });
 };
 
 export const deletePin = async (pinId: string) => {
   // Your SaaS app backend should have an API to delete a pin
+  const url = `https://backend.dataactions.ai/pin?workspace_id=arihant&user_id=userId&pin_id=${pinId}`;
+
+  return fetch(url, {
+    method: "DELETE",
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(
+          `Failed to delete pin with ID ${pinId}: ${response.statusText}`
+        );
+      }
+      return response.json();
+    })
+    .then((data) => data)
+    .catch((err) => {
+      console.error("Error deleting pin:", err);
+      throw err;
+    });
+};
+
+export const AddPin = async (payload: IPin) => {
+  const url = "https://backend.dataactions.ai/pin";
+
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    });
+
+    if (!response.ok) {
+      throw new Error(`Failed to add pin: ${response.statusText}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.error("Error adding pin:", err);
+    throw err;
+  }
 };
