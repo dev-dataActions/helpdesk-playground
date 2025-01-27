1b10d1f64870be47bfd1cc1be95c3755d8d91589
thusharke25 <thushar.ke@dataactions.ai>
2025-01-27 18:50:20 +0530

    fix board page

--- src/modules/insights/pages/BoardPage.jsx
+++ src/modules/insights/pages/BoardPage.jsx
@@ -1,4 +1,4 @@
-import { DashboardLayout, Insight, ValidDashboardColumns } from "da-insight-kit";
+import { Insight, ValidSpanColumns } from "da-insight-kit";
 import useBoard from "../hooks/useBoard";
 
 const BoardPage = ({ workflowId, boardId }) => {
@@ -7,24 +7,22 @@ const BoardPage = ({ workflowId, boardId }) => {
   if (!board) return <p className="mt-10">Board not found.</p>;
 
   return (
-    <div className="p-12 px-40">
+    <div className="px-6 py-4">
       <h1 className="text-2xl mb-3">{board.title}</h1>
-      <div className="flex flex-col gap-y-4">
-        {board?.sections?.map((section, index) => (
-          <div key={index}>
-            <DashboardLayout title={section.title ?? ""} cols={ValidDashboardColumns.TWELVE}>
-              {section.insights.map((insight) => (
-                <Insight
-                  key={insight.id}
-                  title={insight.title}
-                  type={insight.chartType}
-                  metrics={insight.metrics}
-                  filters={insight?.filters}
-                  options={insight?.options}
-                />
-              ))}
-            </DashboardLayout>
-          </div>
+      <div className="grid grid-cols-12 gap-4 pt-2">
+        {board?.insights?.map((insight) => (
+          <Insight
+            key={insight.id}
+            title={insight.title}
+            type={insight.chartType}
+            metrics={insight.metrics}
+            filters={insight?.filters}
+            options={{
+              ...(insight?.options ?? {}),
+              className: "h-64",
+              spanCols: ValidSpanColumns.FOUR,
+            }}
+          />
         ))}
       </div>
     </div>
