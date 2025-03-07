88c56521ec847bfb268b4dc0e7554c72b63ea045
arihant0924 <arihant.s@dataactions.ai>
2025-03-07 14:26:01 +0530

    removed unused code

--- src/modules/insights/services/features.svc.js
+++ src/modules/insights/services/features.svc.js
@@ -2,39 +2,20 @@ const APP_BACKEND_URL = "https://backend.dataactions.ai";
 
 export const getFeatureMapByWorkspaceId = async (workspaceId) => {
   try {
-    const res = await fetch(`${APP_BACKEND_URL}/v3/featuremap?workspace_id=${workspaceId}`);
+    const res = await fetch(
+      `${APP_BACKEND_URL}/v3/featuremap?workspace_id=${workspaceId}`
+    );
     return await res.json();
   } catch (error) {
     console.error(error);
   }
 };
 
-export const getBoardsByFeatureIdAndWorkspaceId = async (featureId, workspaceId) => {
-  try {
-    // const res = await fetch(
-    //   `${APP_BACKEND_URL}/v3/boards?feature_id=${featureId}&workspace_id=${workspaceId}`
-    // );
-    // return await res.json();
-    return [
-      {
-        id: "1",
-        name: "Profits and margins expense",
-        description: "Identifying cost attributes and profitability trends",
-        lastUpdated: "2021-09-01T00:00:00.000Z",
-      },
-      {
-        id: "2",
-        name: "Monthly revenue report",
-        description: "Track monthly revenue and growth",
-        lastUpdated: "2021-09-01T00:00:00.000Z",
-      },
-    ];
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-export const getAppInsightsByFeatureIdAndWorkspaceId = async (appId, featureId, workspaceId) => {
+export const getAppInsightsByFeatureIdAndWorkspaceId = async (
+  appId,
+  featureId,
+  workspaceId
+) => {
   try {
     const res = await fetch(
       `${APP_BACKEND_URL}/v3/app-feature-insight?app_id=${appId}&feature_id=${featureId}&workspace_id=${workspaceId}`
