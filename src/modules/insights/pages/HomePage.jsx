85f49c4dc4fe011055716112c3c783e25dbf6c0f
thusharke25 <dev@dataactions.ai>
2025-07-25 23:24:31 +0530

    setup home page

---
+++ src/modules/insights/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+export const HomePage = () => {
+  return <div>HomePage</div>;
+};
+
+const metricViewConfig = {
+  OUTPUT: [{ metricKey: "", metricLabel }],
+  DRIVER: [{ metricKey: "", metricLabel }],
+  INPUT: [{ metricKey: "", metricLabel }],
+};
--- src/pages/insights/index.jsx
+++ src/pages/insights/index.jsx
@@ -2,6 +2,7 @@ import { useRouter } from "next/router";
 import { DecisionDetailPage } from "../../modules/insights/pages/DecisionDetailPage";
 import { ScreenLayout } from "../../modules/insights/common/layouts/ScreenLayout";
 import { useTenantId } from "../../modules/insights/hooks/useTenantId";
+import { HomePage } from "../../modules/insights/pages/HomePage";
 
 export default function DecisionDetailPageContainer() {
   const router = useRouter();
@@ -15,15 +16,23 @@ export default function DecisionDetailPageContainer() {
     }
   };
 
+  if (router?.query?.decisionId) {
+    return (
+      <ScreenLayout breadcrumbs={[{ name: "Insights" }]}>
+        <DecisionDetailPage
+          tenantId={tenantId}
+          workspaceId={process.env.NEXT_PUBLIC_WORKSPACE_ID}
+          appId={process.env.NEXT_PUBLIC_CFA_APP_ID}
+          decisionId={router?.query?.decisionId}
+          onNavigate={handleNavigate}
+        />
+      </ScreenLayout>
+    );
+  }
+
   return (
     <ScreenLayout breadcrumbs={[{ name: "Insights" }]}>
-      <DecisionDetailPage
-        tenantId={tenantId}
-        workspaceId={process.env.NEXT_PUBLIC_WORKSPACE_ID}
-        appId={process.env.NEXT_PUBLIC_CFA_APP_ID}
-        decisionId={router?.query?.decisionId}
-        onNavigate={handleNavigate}
-      />
+      <HomePage />
     </ScreenLayout>
   );
 }
