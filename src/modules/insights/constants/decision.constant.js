29a3ed52cf4ccb4f45b500933053829a3a795783
dev-dataActions <dev@dataactions.ai>
2025-07-29 16:20:13 +0530

    Update decision.constant.js
    Addedj

--- src/modules/insights/constants/decision.constant.js
+++ src/modules/insights/constants/decision.constant.js
@@ -1,188 +1,149 @@
 export const metricViewConfig = {
   decision_mbmcip2a: {
-    OUTPUT: [
-      {
-        metricKey: "completed_projects",
-        metricLabel: "Qualified Products",
-      },
-      {
-        metricKey: "num_trials_promoted",
-        metricLabel: "Num Trials Advanced",
-      },
-    ],
-    DRIVER: [
-      {
-        metricKey: "trial_completion_rate",
-        metricLabel: "Trial Completion Rate",
-      },
-      {
-        metricKey: "avg_time_to_complete_trial",
-        metricLabel: "Avg. Time to Complete Trial",
-      },
-    ],
-    INPUT: [
-      {
-        metricKey: "no_of_trials_created",
-        metricLabel: "Trials Created",
-      },
-      {
-        metricKey: "num_fields_planted",
-        metricLabel: "Num. Fields Planted",
-      },
-    ],
-  },
-  feature_mbrug9mq: {
-    OUTPUT: [
-      {
-        metricKey: "completed_projects",
-        metricLabel: "Qualified Products",
-      },
-    ],
-    DRIVER: [
-      {
-        metricKey: "trial_completion_rate",
-        metricLabel: "Trial Completion Rate",
-      },
-      {
-        metricKey: "avg_time_to_complete_trial",
-        metricLabel: "Avg. Time to Complete Trial",
-      },
-    ],
-    INPUT: [
-      {
-        metricKey: "no_of_trials_created",
-        metricLabel: "Total Trials Started",
-      },
-    ],
-  },
-  feature_mcyg3aru: {
-    OUTPUT: [
-      {
-        metricKey: "num_trials_promoted",
-        metricLabel: "Total Trials Promoted",
-      },
-    ],
-    DRIVER: [
-      {
-        metricKey: "trial_completion_rate",
-        metricLabel: "Trial Completion Rate",
-      },
-      {
-        metricKey: "avg_time_to_complete_trial",
-        metricLabel: "Avg. Time to Complete Trial",
-      },
-    ],
-    INPUT: [
-      {
-        metricKey: "no_of_trials_created",
-        metricLabel: "Total Trials Started",
-      },
-      {
-        metricKey: "num_trials_created",
-        metricLabel: "Total Trials Closed",
-      },
-    ],
-  },
-  feature_mcygu7lb: {
-    OUTPUT: [
-      {
-        metricKey: "num_fields_harvested",
-        metricLabel: "No of Trials Harvested",
-      },
-    ],
-    DRIVER: [
-      {
-        metricKey: "fieldbook_fill_rate",
-        metricLabel: "FieldBook Fill Rate",
-      },
-      {
-        metricKey: "avg_delay_in_planting",
-        metricLabel: "Avg. Delay in Planting",
-      },
-      {
-        metricKey: "avg_delay_in_observations",
-        metricLabel: "Avg. Delay in Observation Protocol",
-      },
-      {
-        metricKey: "avg_delay_in_agronomy_protocol",
-        metricLabel: "Avg. Delay in Agronomy Protocol",
-      },
-      {
-        metricKey: "avg_delay_in_harvest",
-        metricLabel: "Avg. Delay in Harvest",
-      },
-    ],
-    INPUT: [
-      {
-        metricKey: "num_fields_planted",
-        metricLabel: "No of Trials Planted",
-      },
-    ],
-  },
+  OUTPUT: [
+    { metricKey: "no_of_trials_completed", metricLabel: "No of Trials Completed" },
+    { metricKey: "no_of_entries_advanced", metricLabel: "No of Entries Advanced" }
+  ],
+  DRIVER: [
+    { metricKey: "trial_completion_rate", metricLabel: "Trial Completion Rate" },
+    { metricKey: "fieldbook_fill_rate", metricLabel: "Fieldbook Fill Rate" },
+    { metricKey: "avg_time_to_complete_trial", metricLabel: "Avg. Time to Complete Trial" }
+  ],
+  INPUT: [
+    { metricKey: "num_fields_harvested", metricLabel: "Num. Fields Harvested" },
+    { metricKey: "num_fields_planted", metricLabel: "Num. Fields Planted" },
+    { metricKey: "total_trials_planned", metricLabel: "Total Trials Planned" }
+  ]
+},
+
+feature_mbrug9mq: {
+  OUTPUT: [
+    { metricKey: "no_of_trials_completed", metricLabel: "No of Trials Completed" }
+  ],
+  DRIVER: [
+    { metricKey: "trial_completion_rate", metricLabel: "Trial Completion Rate" },
+    { metricKey: "fieldbook_fill_rate", metricLabel: "Fieldbook Fill Rate" },
+    { metricKey: "avg_time_to_complete_trial", metricLabel: "Avg. Time to Complete Trial" }
+  ],
+  INPUT: [
+    { metricKey: "num_fields_harvested", metricLabel: "Num. Fields Harvested" },
+    { metricKey: "num_fields_planted", metricLabel: "Num. Fields Planted" },
+    { metricKey: "total_trials_planned", metricLabel: "Total Trials Planned" }
+  ]
+},
+
+feature_mcyg3aru: {
+  OUTPUT: [
+    { metricKey: "no_of_trials_completed", metricLabel: "No of Trials Completed" }
+  ],
+  DRIVER: [
+    { metricKey: "trial_completion_rate", metricLabel: "Trial Completion Rate" },
+    { metricKey: "planting_to_harvest_completion_rate", metricLabel: "Planting to Harvest Completion Rate" },
+    { metricKey: "avg_time_to_complete_trial", metricLabel: "Avg. Time to Complete Trial" }
+  ],
+  INPUT: [
+    { metricKey: "num_fields_harvested", metricLabel: "Num. Fields Harvested" },
+    { metricKey: "num_fields_planted", metricLabel: "Num. Fields Planted" },
+    { metricKey: "total_trials_planned", metricLabel: "Total Trials Planned" }
+  ]
+},
+
+feature_mcygu7lb: {
+  OUTPUT: [
+    { metricKey: "no_of_fields_harvested", metricLabel: "No of Fields Harvested" },
+    // { metricKey: "avg_time_to_complete_harvest", metricLabel: "Avg. Time to Complete Harvest" }
+  ],
+  DRIVER: [
+    { metricKey: "fieldbook_fill_rate", metricLabel: "Fieldbook Fill Rate" },
+    { metricKey: "avg_delay_in_observations", metricLabel: "Avg. Delay in Observations" },
+    { metricKey: "avg_delay_in_agronomy_protocol", metricLabel: "Avg. Delay in Agronomy Protcol" },
+    { metricKey: "avg_delay_in_planting", metricLabel: "Avg. Delay in Planting" },
+    { metricKey: "avg_delay_in_harvest", metricLabel: "Avg. Delay in Harvest" }
+  ],
+  INPUT: [
+    { metricKey: "num_fields_planted", metricLabel: "Num. Fields Planted" }
+  ]
 };
 
 export const explanationInightsConfig = {
-  decision_mbmcip2a: [
+  overview_lever: [
     {
       payload: {
         fromtime: "2025-02-01",
         totime: "2025-07-31",
-        metric_name: "completed_projects",
-        timegrain: "month",
-        filters: [],
-      },
-      insight_type: "summary",
-      workspace_id: "workspace_99398cfd-dfd4-4195-8702-82dcfdd4efac",
-      tenant_id: null,
-    },
-    {
-      payload: {
-        fromtime: "2025-02-01",
-        totime: "2025-07-31",
-        metric_name: "num_trials_promoted",
-        timegrain: "month",
-        filters: [],
-      },
-      insight_type: "summary",
-      workspace_id: "workspace_99398cfd-dfd4-4195-8702-82dcfdd4efac",
-      tenant_id: null,
-    },
-    {
-      payload: {
-        fromtime: "2025-02-01",
-        totime: "2025-07-31",
-        metric_name: "num_trials_promoted",
+        metric_name: "no_of_entries_advanced",
         timegrain: "month",
         filters: [],
       },
       insight_type: "trend",
       workspace_id: "workspace_99398cfd-dfd4-4195-8702-82dcfdd4efac",
-      tenant_id: null,
+      tenant_id: "mcsyjvco",
     },
     {
       payload: {
         fromtime: "2025-02-01",
         totime: "2025-07-31",
-        metric_name: "num_trials_promoted",
+        metric_name: "no_of_entries_advanced",
+        timegrain: "month",
+        dimensions: ["crop_name"],
+        filters: [],
+      },
+      insight_type: "contributor",
+      workspace_id: "workspace_99398cfd-dfd4-4195-8702-82dcfdd4efac",
+      tenant_id: "mcsyjvco",
+    },
+    {
+      payload: {
+        fromtime: "2025-02-01",
+        totime: "2025-07-31",
+        metric_name: "no_of_entries_advanced",
         timegrain: "month",
         dimensions: ["user_region"],
         filters: [],
       },
       insight_type: "contributor",
       workspace_id: "workspace_99398cfd-dfd4-4195-8702-82dcfdd4efac",
-      tenant_id: null,
+      tenant_id: "mcsyjvco",
+    },
+  ],
+  decision_mbmcip2a: [
+    {
+      payload: {
+        fromtime: "2025-02-01",
+        totime: "2025-07-31",
+        metric_name: "no_of_trials_completed",
+        timegrain: "month",
+        filters: [],
+      },
+      insight_type: "trend",
+      workspace_id: "workspace_99398cfd-dfd4-4195-8702-82dcfdd4efac",
+      tenant_id: "mcsyjvco",
     },
     {
       payload: {
         fromtime: "2025-02-01",
         totime: "2025-07-31",
-        metric_name: "num_trials_promoted",
-        dimensions: ["trial_name"],
+        metric_name: "no_of_trials_completed",
+        timegrain: "month",
+        dimensions: ["crop_name"],
         filters: [],
       },
       insight_type: "contributor",
       workspace_id: "workspace_99398cfd-dfd4-4195-8702-82dcfdd4efac",
-      tenant_id: null,
+      tenant_id: "mcsyjvco",
+    },
+    {
+      payload: {
+        fromtime: "2025-02-01",
+        totime: "2025-07-31",
+        metric_name: "no_of_trials_completed",
+        timegrain: "month",
+        dimensions: ["user_region"],
+        filters: [],
+      },
+      insight_type: "contributor",
+      workspace_id: "workspace_99398cfd-dfd4-4195-8702-82dcfdd4efac",
+      tenant_id: "mcsyjvco",
     },
   ],
   feature_mbrug9mq: [
@@ -190,13 +151,39 @@ export const explanationInightsConfig = {
       payload: {
         fromtime: "2025-02-01",
         totime: "2025-07-31",
-        metric_name: "completed_projects",
+        metric_name: "no_of_trials_completed",
         timegrain: "month",
         filters: [],
       },
-      insight_type: "summary",
+      insight_type: "trend",
       workspace_id: "workspace_99398cfd-dfd4-4195-8702-82dcfdd4efac",
-      tenant_id: null,
+      tenant_id: "mcsyjvco",
+    },
+    {
+      payload: {
+        fromtime: "2025-02-01",
+        totime: "2025-07-31",
+        metric_name: "no_of_trials_completed",
+        timegrain: "month",
+        dimensions: ["crop_name"],
+        filters: [],
+      },
+      insight_type: "contributor",
+      workspace_id: "workspace_99398cfd-dfd4-4195-8702-82dcfdd4efac",
+      tenant_id: "mcsyjvco",
+    },
+    {
+      payload: {
+        fromtime: "2025-02-01",
+        totime: "2025-07-31",
+        metric_name: "no_of_trials_completed",
+        timegrain: "month",
+        dimensions: ["user_region"],
+        filters: [],
+      },
+      insight_type: "contributor",
+      workspace_id: "workspace_99398cfd-dfd4-4195-8702-82dcfdd4efac",
+      tenant_id: "mcsyjvco",
     },
   ],
   feature_mcyg3aru: [
@@ -204,50 +191,39 @@ export const explanationInightsConfig = {
       payload: {
         fromtime: "2025-02-01",
         totime: "2025-07-31",
-        metric_name: "num_trials_promoted",
-        timegrain: "month",
-        filters: [],
-      },
-      insight_type: "summary",
-      workspace_id: "workspace_99398cfd-dfd4-4195-8702-82dcfdd4efac",
-      tenant_id: null,
-    },
-    {
-      payload: {
-        fromtime: "2025-02-01",
-        totime: "2025-07-31",
-        metric_name: "num_trials_promoted",
+        metric_name: "no_of_trials_completed",
         timegrain: "month",
         filters: [],
       },
       insight_type: "trend",
       workspace_id: "workspace_99398cfd-dfd4-4195-8702-82dcfdd4efac",
-      tenant_id: null,
+      tenant_id: "mcsyjvco",
     },
     {
       payload: {
         fromtime: "2025-02-01",
         totime: "2025-07-31",
-        metric_name: "num_trials_promoted",
+        metric_name: "no_of_trials_completed",
         timegrain: "month",
-        dimensions: ["user_region"],
+        dimensions: ["trial_location"],
         filters: [],
       },
       insight_type: "contributor",
       workspace_id: "workspace_99398cfd-dfd4-4195-8702-82dcfdd4efac",
-      tenant_id: null,
+      tenant_id: "mcsyjvco",
     },
     {
       payload: {
         fromtime: "2025-02-01",
         totime: "2025-07-31",
-        metric_name: "num_trials_promoted",
-        dimensions: ["trial_name"],
+        metric_name: "no_of_trials_completed",
+        timegrain: "month",
+        dimensions: ["zone"],
         filters: [],
       },
       insight_type: "contributor",
       workspace_id: "workspace_99398cfd-dfd4-4195-8702-82dcfdd4efac",
-      tenant_id: null,
+      tenant_id: "mcsyjvco",
     },
   ],
   feature_mcygu7lb: [
@@ -255,38 +231,40 @@ export const explanationInightsConfig = {
       payload: {
         fromtime: "2025-02-01",
         totime: "2025-07-31",
-        metric_name: "num_fields_harvested",
-        timegrain: "month",
-        filters: [],
-      },
-      insight_type: "summary",
-      workspace_id: "workspace_99398cfd-dfd4-4195-8702-82dcfdd4efac",
-      tenant_id: null,
-    },
-    {
-      payload: {
-        fromtime: "2025-02-01",
-        totime: "2025-07-31",
-        metric_name: "num_fields_harvested",
+        metric_name: "no_of_fields_harvested",
         timegrain: "month",
         filters: [],
       },
       insight_type: "trend",
       workspace_id: "workspace_99398cfd-dfd4-4195-8702-82dcfdd4efac",
-      tenant_id: null,
+      tenant_id: "mcsyjvco",
     },
     {
       payload: {
         fromtime: "2025-02-01",
         totime: "2025-07-31",
-        metric_name: "num_fields_harvested",
+        metric_name: "no_of_fields_harvested",
         timegrain: "month",
-        dimensions: ["district"],
+        dimensions: ["crop_name"],
         filters: [],
       },
       insight_type: "contributor",
       workspace_id: "workspace_99398cfd-dfd4-4195-8702-82dcfdd4efac",
-      tenant_id: null,
+      tenant_id: "mcsyjvco",
+    },
+    {
+      payload: {
+        fromtime: "2025-02-01",
+        totime: "2025-07-31",
+        metric_name: "no_of_fields_harvested",
+        timegrain: "month",
+        dimensions: ["trial_location"],
+        filters: [],
+      },
+      insight_type: "contributor",
+      workspace_id: "workspace_99398cfd-dfd4-4195-8702-82dcfdd4efac",
+      tenant_id: "mcsyjvco",
     },
   ],
 };
+
