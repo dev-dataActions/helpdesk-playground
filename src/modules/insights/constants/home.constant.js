65c88d0cb345c77c94317220e147819f0a1ab08e
dev-dataActions <dev@dataactions.ai>
2025-07-29 16:17:27 +0530

    Update home.constant.js
    Added

--- src/modules/insights/constants/home.constant.js
+++ src/modules/insights/constants/home.constant.js
@@ -1,48 +1,12 @@
 export const metricViewConfig = {
-  OUTPUT: [
-    {
-      metricKey: "completed_projects",
-      metricLabel: "Qualified Products",
-    },
-    {
-      metricKey: "num_trials_promoted",
-      metricLabel: "Trials Promoted",
-    },
-    {
-      metricKey: "num_fields_harvested",
-      metricLabel: "Fields Harvested",
-    },
-  ],
-  DRIVER: [
-    {
-      metricKey: "trial_completion_rate",
-      metricLabel: "Trial Completion Rate",
-    },
-    {
-      metricKey: "avg_time_to_complete_trial",
-      metricLabel: "Avg. Time to Complete Trial",
-    },
-    {
-      metricKey: "avg_delay_in_planting",
-      metricLabel: "Avg. Delay in Planting",
-    },
-    {
-      metricKey: "avg_delay_in_harvest",
-      metricLabel: "Avg. Delay in Harvest",
-    },
+    OUTPUT: [
+    { metricKey: "no_of_entries_advanced", metricLabel: "No of Entries Advanced" }
   ],
   INPUT: [
-    {
-      metricKey: "no_of_trials_created",
-      metricLabel: "Trials Created",
-    },
-    {
-      metricKey: "fieldbook_fill_rate",
-      metricLabel: "Fieldbook Fill Rate",
-    },
-    {
-      metricKey: "num_fields_planted",
-      metricLabel: "Fields Planted",
-    },
-  ],
+    { metricKey: "no_of_trials_completed", metricLabel: "No of Trials Completed" },
+    { metricKey: "no_of_trials_started", metricLabel: "No of Trials Started" },
+    { metricKey: "trial_completion_rate", metricLabel: "Trial Completion Rate" },
+    { metricKey: "avg_time_to_complete_trial", metricLabel: "Avg. Time to Complete Trial" }
+  ]
+}
 };
